ddec16ddee72fca17677bce4ca867ff04044d224
Orkun24 <orkun.yilmaz23@gmail.com>
2025-08-05 14:19:06 +0300

    orkun

---
+++ ProjectIdle/Assets/Scripts/Missions/MissionConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum MissionType
+{
+    ReachProductLevel,
+    UnlockProduct,
+    ReachTotalMoney
+}
+
+[System.Serializable]
+public class MissionCondition
+{
+    public MissionType type;
+
+    [Header("Bağlı Ürün")]
+    public ProductConfig targetProduct;  // 👈 Doğrudan config referansı
+
+    [Header("Hedefler")]
+    public int targetLevel;
+    public double targetMoney;
+}
+
+[CreateAssetMenu(fileName = "NewMission", menuName = "Missions/Mission")]
+public class MissionConfig : ScriptableObject
+{
+    public string missionName;
+    public List<MissionCondition> conditions = new List<MissionCondition>();
+    public int rewardGems = 10;
+    public bool completed = false;
+}
---
+++ ProjectIdle/Assets/Scripts/Missions/MissionManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    [Header("Tüm Görevler")]
+    public List<MissionConfig> missions;
+
+    void Update()
+    {
+        foreach (var mission in missions)
+        {
+            if (mission.completed)
+                continue;
+
+            bool allMet = true;
+
+            foreach (var condition in mission.conditions)
+            {
+                if (!IsConditionMet(condition))
+                {
+                    allMet = false;
+                    break;
+                }
+            }
+
+            if (allMet)
+            {
+                CompleteMission(mission);
+            }
+        }
+    }
+
+    bool IsConditionMet(MissionCondition condition)
+    {
+        switch (condition.type)
+        {
+            case MissionType.ReachProductLevel:
+                {
+                    var product = IncomeManager.Instance.products
+                        .Find(p => p.config == condition.targetProduct);
+                    return product != null && product.level >= condition.targetLevel;
+                }
+
+            case MissionType.UnlockProduct:
+                {
+                    var product = IncomeManager.Instance.products
+                        .Find(p => p.config == condition.targetProduct);
+                    return product != null && product.uiObject != null && product.uiObject.activeSelf;
+                }
+
+            case MissionType.ReachTotalMoney:
+                return IncomeManager.Instance.totalMoney >= condition.targetMoney;
+
+            default:
+                return false;
+        }
+    }
+
+
+    void CompleteMission(MissionConfig mission)
+    {
+        mission.completed = true;
+        Debug.Log($"✅ Görev tamamlandı: {mission.missionName}");
+
+        // Ödül ver (örnek: elmas)
+        int currentGems = PlayerPrefs.GetInt("Gems", 0);
+        PlayerPrefs.SetInt("Gems", currentGems + mission.rewardGems);
+
+        // TODO: UI bildirimi, animasyon, ses efekti vb.
+    }
+}

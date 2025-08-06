2d45929072f7d7352320a441e4e8391c719d1996
Orkun24 <orkun.yilmaz23@gmail.com>
2025-08-06 11:08:58 +0300

    orkun

---
+++ ProjectIdle/Assets/Scripts/Incomes/DiamondIncome.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DiamondIncome : MonoBehaviour
+{
+    public static DiamondIncome Instance;
+
+    [Header("UI")]
+    public TextMeshProUGUI diamondText;
+
+    private int diamonds = 0;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        LoadDiamonds();
+        UpdateUI();
+    }
+
+    public void AddDiamonds(int amount)
+    {
+        diamonds += amount;
+        SaveDiamonds();
+        UpdateUI();
+        Debug.Log($"💎 {amount} elmas eklendi. Toplam: {diamonds}");
+    }
+
+    public bool SpendDiamonds(int amount)
+    {
+        if (diamonds >= amount)
+        {
+            diamonds -= amount;
+            SaveDiamonds();
+            UpdateUI();
+            Debug.Log($"💎 {amount} elmas harcandı. Kalan: {diamonds}");
+            return true;
+        }
+        else
+        {
+            Debug.Log("❌ Yetersiz elmas.");
+            return false;
+        }
+    }
+
+    public void UpdateUI()
+    {
+        if (diamondText != null)
+            diamondText.text = diamonds.ToString();
+    }
+
+    private void SaveDiamonds()
+    {
+        PlayerPrefs.SetInt("Diamonds", diamonds);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadDiamonds()
+    {
+        diamonds = PlayerPrefs.GetInt("Diamonds", 0);
+    }
+
+    public int GetDiamondCount()
+    {
+        return diamonds;
+    }
+}

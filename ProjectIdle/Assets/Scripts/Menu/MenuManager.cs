b713c9e0638041038732905e403a6f190aba0fa5
mertsyh <2300007247@stu.iku.edu.tr>
2025-10-15 23:15:52 +0300

    son

--- ProjectIdle/Assets/Scripts/Menu/MenuManager.cs
+++ ProjectIdle/Assets/Scripts/Menu/MenuManager.cs
@@ -4,34 +4,52 @@ public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance;
 
+    // 1. ADIM: Inspector'dan atayacaðýn kapatma panelini ekle
+    public GameObject closePanel;
+
     private MenuToggleButton currentlyOpenButton;
 
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
-        else
-            Destroy(gameObject);
+     
     }
+
+   
 
     // Butonlar burayý çaðýracak
     public void ToggleButton(MenuToggleButton clickedButton)
     {
+        // Eðer baþka bir buton açýksa, önce onu kapat
         if (currentlyOpenButton != null && currentlyOpenButton != clickedButton)
         {
             currentlyOpenButton.CloseSlider();
         }
 
+        // Týklanan butonun durumunu deðiþtir
         if (currentlyOpenButton == clickedButton)
         {
-            // Ayný butona tekrar basýlýrsa kapat
-            clickedButton.CloseSlider();
-            currentlyOpenButton = null;
+            // Ayný butona tekrar basýlýrsa kapat (Yeni fonksiyonu çaðýrýyoruz)k
+            CloseCurrentlyOpenMenu();
         }
         else
         {
+            // Yeni butonu aç
             clickedButton.OpenSlider();
             currentlyOpenButton = clickedButton;
+         
+        }
+    }
+
+    // 2. ADIM: Boþ alan paneli bu fonksiyonu çaðýracak
+    public void CloseCurrentlyOpenMenu()
+    {
+        if (currentlyOpenButton != null)
+        {
+            currentlyOpenButton.CloseSlider();
+            currentlyOpenButton = null;
+          
         }
     }
 }
--- ProjectIdle/Assets/Scripts/Menu/MenuToggleButton.cs
+++ ProjectIdle/Assets/Scripts/Menu/MenuToggleButton.cs
@@ -1,3 +1,5 @@
+
+
 using UnityEngine;
 
 public class MenuToggleButton : MonoBehaviour
@@ -6,16 +8,17 @@ public class MenuToggleButton : MonoBehaviour
 
     [HideInInspector] public bool isOpen = false;
 
-    // MenuManager üzerinden çaðrýlacak
+    
     public void OnToggleMenu()
     {
+      
         if (MenuManager.Instance != null)
         {
             MenuManager.Instance.ToggleButton(this);
         }
     }
 
-    // Slider açmak için
+  
     public void OpenSlider()
     {
         if (animator != null && !isOpen)
@@ -25,13 +28,17 @@ public class MenuToggleButton : MonoBehaviour
         }
     }
 
-    // Slider kapatmak için
     public void CloseSlider()
     {
+       
+
         if (animator != null && isOpen)
         {
+         
             animator.SetTrigger("Close");
             isOpen = false;
         }
+       
+     
     }
 }
